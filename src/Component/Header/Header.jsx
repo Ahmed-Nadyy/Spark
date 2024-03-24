0820b15c0fd67652630116b8e694e9d7c69eb338
Ahmed Nady <120312236+NAADDY@users.noreply.github.com>
2024-03-24 23:03:24 +0200

    Khaled merge

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,30 @@
+import "./header.css";
+import logo from "../../assets/flogo.png"
+import NavItems from "./NavItems";
+
+const Header = () => {
+
+  return (
+    <header>
+      <div className="container flex">
+        <div className="logo">
+          <img width="65%" src={logo} alt="" />
+        </div>
+        <div className="nav-items flex">
+          <NavItems itemName="Home" />
+          <NavItems itemName="Courses" />
+          <NavItems itemName="Blog" />
+          <NavItems itemName="Events" />
+          <NavItems itemName="Monitors" />
+          <NavItems itemName="Testimonials" />
+        </div>
+        <div className="btns flex">
+          <button className="login">Login</button>
+          <button className="signup">Signup</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Thrd from './3rd/Thrd'
 import Frth from './Frth/Frth'
+import Courses from '../Courses/courses'
+import Hero from '../Hero/Hero'
+import Header from '../Header/Header'
 
 export default function Home() {
   return (
     <>
-    
+      {/* <header /> */}
+      <Header />
+      <Hero />
+      <Courses />
       <Thrd />
       <Frth />
      
